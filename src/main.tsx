fe3d2c0348f4c3389bb64ed5ed86e2f51cb9121e
ferdin <ferdnorbert@gmail.com>
2026-08-24 16:16:28 -0400

    add axios api and google client id

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,13 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.tsx";
 import { store } from "./app/store.ts";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <GoogleOAuthProvider clientId="387497052520-15s98jerl3c6lkl1djes1n7e1brmh7l5.apps.googleusercontent.com">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </GoogleOAuthProvider>
   </StrictMode>,
 );
