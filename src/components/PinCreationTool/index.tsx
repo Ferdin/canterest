9eb4caf9231f33af75a099336f2de09cd8e3c745
ferdin <ferdnorbert@gmail.com>
2026-09-08 14:38:55 -0400

    pintool right menu

--- src/components/PinCreationTool/index.tsx
+++ src/components/PinCreationTool/index.tsx
@@ -1,4 +1,4 @@
-import { Images } from "lucide-react";
+import { Folder, Images, Plus, X } from "lucide-react";
 import UploadMediaTextInput from "../PinCreationToolElements/UploadMediaElements/UploadMediaTextInput";
 import UploadMediaTextArea from "../PinCreationToolElements/UploadMediaElements/UploadMediaTextArea";
 import UploadMediaGenericComboBox,{ type ComboBoxItem } from "../PinCreationToolElements/UploadMediaElements/UploadMediaGenericComboBox";
@@ -26,135 +26,173 @@ const topics: Topic[] = [
 export default function PinCreationTool(){
     const [selectedBoard, setSelectedBoard] = useState<Board | null>(null);
     const [selectedTopics, setSelectedTopics] = useState<Topic[]>([]);
+    const [selectPins, setSelectPins] = useState<boolean>(false);
+
+    const handleSelectPins = () => {
+        setSelectPins((prev) => !prev)
+    }
+
     return (
-    <div className="px-56 pt-10 flex flex-row gap-8 mb-4">
-        <div>
-            <div className="relative w-96 h-96 bg-olive-300 flex border rounded-2xl py-10 cursor-pointer">
-                <div className="flex flex-col items-center justify-center w-full h-full pb-12">
-                    <Images/>
-                    <span className="font-semibold mt-2">Upload your media</span>
-                    <p className="text-center text-sm text-gray-500 pt-2">Select multiple files in your file<br/>picker with Shift or Cmd/Ctrl.</p>
-                    <p className="absolute bottom-6 left-1/2 -translate-x-1/2 text-center text-sm text-gray-500 w-full px-4">JPG, PNG up to 20MB · MP4 up to 200MB</p>
-                </div>
+    <div className="flex flex-row min-h-screen">
+        <div className={`${selectPins ? "w-[80%]" : "w-[95%]"} `}>
+            <div className="flex justify-between items-center px-2 py-4 border-b-olive-300 border-b">
+                <h2 className="font-bold text-2xl">Create Pin</h2>
+                <button className="bg-red-600 text-white px-4 py-2 hover:bg-red-700 cursor-pointer font-semibold rounded-lg">Publish</button>
             </div>
-            <hr className="border-olive-300 my-6  w-96" />
-            <div className="w-96 h-12 bg-olive-300 flex border rounded-2xl cursor-pointer hover:bg-olive-400">
-                <div className="flex items-center justify-center w-full h-full text-sm font-semibold">
-                    Save from URL
-                </div>
-            </div>
-        </div>
-        <div className="flex flex-col gap-6">
-            <UploadMediaTextInput name="can_pin_title" placeholder="Tell everyone what your Pin is about" id="can_pin_title" labelText="Title"/>
-            <UploadMediaTextArea name="can_pin_desc" placeholder="Describe your Pin" id="can_pin_desc" labelText="Description"/>
-            <UploadMediaTextInput name="can_pin_link" placeholder="Add a Link" id="can_pin_link" labelText="Link"/>
-            <UploadMediaGenericComboBox
-                items={boards}
-                label="Board"
-                name="can_pin_board"
-                id="can_pin_board"
-                placeholder="Search for a board."
-                value={selectedBoard}
-                onChange={(value) => setSelectedBoard(value as Board | null)}
-                onCreateNew={(query) => {
-                    // POST /boards, then push the new board into `boards`
-                    console.log("Create new board:", query);
-                }}
-                createLabel={() => "Create new board"}
-                renderIcon={() => <span>📌</span>}
-            />
-            <UploadMediaGenericComboBox
-                items={topics}
-                label="Topics"
-                name="tagged_topics"
-                id="tagged_topics"
-                placeholder="Search for topics to tag."
-                multiple
-                value={selectedTopics}
-                onChange={(value) => setSelectedTopics(value as Topic[])}
-                onCreateNew={(query) => {
-                    console.log("Create new topic:", query);
-                }}
-                createLabel={(query) => `Add "${query}" as a topic`}
-                renderIcon={() => <span>#</span>}
-            />
-            <div className="flex flex-col">
-                <span className="text-xs font-semibold">Tag Products</span>
-                <button className="text-sm font-semibold bg-olive-200 py-1 w-48 rounded-lg mt-2">Add Products</button>
-            </div>
-            <div className="flex flex-row gap-2">
+            <div className="px-56 pt-10 flex flex-row gap-8 mb-4">
                 <div>
-                    <label className="relative inline-block w-15 h-8.5">
-                        <input type="checkbox" className="peer opacity-0 w-0 h-0" />
-                        <span className="absolute cursor-pointer inset-0 bg-gray-300 transition-all duration-400 rounded-full
-                                    peer-checked:bg-blue-500
-                                    peer-focus:shadow-[0_0_1px_#2196F3]
-                                    before:content-[''] before:absolute before:h-6.5 before:w-6.5 before:left-1 before:bottom-1
-                                    before:bg-white before:transition-all before:duration-400 before:rounded-full
-                                    peer-checked:before:translate-x-6.5">
-                        </span>
-                    </label>
+                    <div className="relative w-96 h-96 bg-olive-300 flex border rounded-2xl py-10 cursor-pointer">
+                        <div className="flex flex-col items-center justify-center w-full h-full pb-12">
+                            <Images/>
+                            <span className="font-semibold mt-2">Upload your media</span>
+                            <p className="text-center text-sm text-gray-500 pt-2">Select multiple files in your file<br/>picker with Shift or Cmd/Ctrl.</p>
+                            <p className="absolute bottom-6 left-1/2 -translate-x-1/2 text-center text-sm text-gray-500 w-full px-4">JPG, PNG up to 20MB · MP4 up to 200MB</p>
+                        </div>
+                    </div>
+                    <hr className="border-olive-300 my-6  w-96" />
+                    <div className="w-96 h-12 bg-olive-300 flex border rounded-2xl cursor-pointer hover:bg-olive-400">
+                        <div className="flex items-center justify-center w-full h-full text-sm font-semibold">
+                            Save from URL
+                        </div>
+                    </div>
                 </div>
-                <div className="flex flex-col gap-2">
-                    <h3 className="font-semibold mt-1">Mark as AI-Modified</h3>
-                    <span className="text-sm text-olive-500">Content that was made completely or partly with AI</span>
-                    <div className="flex gap-2">
-                        <input type="checkbox"/>
-                        <span className="text-sm">This Pin includes an AI-generated person</span>
+                <div className="flex flex-col gap-6">
+                    <UploadMediaTextInput name="can_pin_title" placeholder="Tell everyone what your Pin is about" id="can_pin_title" labelText="Title"/>
+                    <UploadMediaTextArea name="can_pin_desc" placeholder="Describe your Pin" id="can_pin_desc" labelText="Description"/>
+                    <UploadMediaTextInput name="can_pin_link" placeholder="Add a Link" id="can_pin_link" labelText="Link"/>
+                    <UploadMediaGenericComboBox
+                        items={boards}
+                        label="Board"
+                        name="can_pin_board"
+                        id="can_pin_board"
+                        placeholder="Search for a board."
+                        value={selectedBoard}
+                        onChange={(value) => setSelectedBoard(value as Board | null)}
+                        onCreateNew={(query) => {
+                            // POST /boards, then push the new board into `boards`
+                            console.log("Create new board:", query);
+                        }}
+                        createLabel={() => "Create new board"}
+                        renderIcon={() => <span>📌</span>}
+                        />
+                    <UploadMediaGenericComboBox
+                        items={topics}
+                        label="Topics"
+                        name="tagged_topics"
+                        id="tagged_topics"
+                        placeholder="Search for topics to tag."
+                        multiple
+                        value={selectedTopics}
+                        onChange={(value) => setSelectedTopics(value as Topic[])}
+                        onCreateNew={(query) => {
+                            console.log("Create new topic:", query);
+                        }}
+                        createLabel={(query) => `Add "${query}" as a topic`}
+                        renderIcon={() => <span>#</span>}
+                        />
+                    <div className="flex flex-col">
+                        <span className="text-xs font-semibold">Tag Products</span>
+                        <button className="text-sm font-semibold bg-olive-200 py-1 w-48 rounded-lg mt-2">Add Products</button>
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <div>
+                            <label className="relative inline-block w-15 h-8.5">
+                                <input type="checkbox" className="peer opacity-0 w-0 h-0" />
+                                <span className="absolute cursor-pointer inset-0 bg-gray-300 transition-all duration-400 rounded-full
+                                            peer-checked:bg-blue-500
+                                            peer-focus:shadow-[0_0_1px_#2196F3]
+                                            before:content-[''] before:absolute before:h-6.5 before:w-6.5 before:left-1 before:bottom-1
+                                            before:bg-white before:transition-all before:duration-400 before:rounded-full
+                                            peer-checked:before:translate-x-6.5">
+                                </span>
+                            </label>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <h3 className="font-semibold mt-1">Mark as AI-Modified</h3>
+                            <span className="text-sm text-olive-500">Content that was made completely or partly with AI</span>
+                            <div className="flex gap-2">
+                                <input type="checkbox"/>
+                                <span className="text-sm">This Pin includes an AI-generated person</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <div>
+                            <label className="relative inline-block w-15 h-8.5">
+                                <input type="checkbox" className="peer opacity-0 w-0 h-0" />
+                                <span className="absolute cursor-pointer inset-0 bg-gray-300 transition-all duration-400 rounded-full
+                                            peer-checked:bg-blue-500
+                                            peer-focus:shadow-[0_0_1px_#2196F3]
+                                            before:content-[''] before:absolute before:h-6.5 before:w-6.5 before:left-1 before:bottom-1
+                                            before:bg-white before:transition-all before:duration-400 before:rounded-full
+                                            peer-checked:before:translate-x-6.5">
+                                </span>
+                            </label>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <h3 className="font-semibold mt-1">Allow people to comment</h3>
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-2 w-lg">
+                        <div>
+                            <label className="relative inline-block w-15 h-8.5">
+                                <input type="checkbox" className="peer opacity-0 w-0 h-0" />
+                                <span className="absolute cursor-pointer inset-0 bg-gray-300 transition-all duration-400 rounded-full
+                                            peer-checked:bg-blue-500
+                                            peer-focus:shadow-[0_0_1px_#2196F3]
+                                            before:content-[''] before:absolute before:h-6.5 before:w-6.5 before:left-1 before:bottom-1
+                                            before:bg-white before:transition-all before:duration-400 before:rounded-full
+                                            peer-checked:before:translate-x-6.5">
+                                </span>
+                            </label>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <h3 className="font-semibold mt-1">Show similar products</h3>
+                            <p className="text-sm text-olive-500">People can shop products similar to what's shown in this Pin using visual search
+                            Shopping recommendations aren't available for Idea ads and Pins with tagged products or paid partnership label</p>
+                        </div>
+                    </div>
+                    <div className="relative w-lg">
+                        <textarea 
+                            name="can_pin_alt_desc" 
+                            placeholder="Describe your Pin's visual details"
+                            id="can_pin_alt_desc"
+                            className="peer border-gray-300 border rounded-2xl h-18 w-full px-4 pt-7 pb-1 outline-none text-base transition-all resize-none"
+                            />
+                        <label
+                            htmlFor="can_pin_alt_desc"
+                            className="absolute left-4 top-2 text-xs font-semibold text-gray-80000 pointer-events-none"
+                            >
+                            Alt Text
+                        </label>
+                        <p className="text-xs w-full text-center">This helps people using screen readers understand what your Pin is about</p>
                     </div>
                 </div>
             </div>
-            <div className="flex flex-row gap-2">
-                <div>
-                    <label className="relative inline-block w-15 h-8.5">
-                        <input type="checkbox" className="peer opacity-0 w-0 h-0" />
-                        <span className="absolute cursor-pointer inset-0 bg-gray-300 transition-all duration-400 rounded-full
-                                    peer-checked:bg-blue-500
-                                    peer-focus:shadow-[0_0_1px_#2196F3]
-                                    before:content-[''] before:absolute before:h-6.5 before:w-6.5 before:left-1 before:bottom-1
-                                    before:bg-white before:transition-all before:duration-400 before:rounded-full
-                                    peer-checked:before:translate-x-6.5">
-                        </span>
-                    </label>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <h3 className="font-semibold mt-1">Allow people to comment</h3>
-                </div>
-            </div>
-            <div className="flex flex-row gap-2 w-lg">
-                <div>
-                    <label className="relative inline-block w-15 h-8.5">
-                        <input type="checkbox" className="peer opacity-0 w-0 h-0" />
-                        <span className="absolute cursor-pointer inset-0 bg-gray-300 transition-all duration-400 rounded-full
-                                    peer-checked:bg-blue-500
-                                    peer-focus:shadow-[0_0_1px_#2196F3]
-                                    before:content-[''] before:absolute before:h-6.5 before:w-6.5 before:left-1 before:bottom-1
-                                    before:bg-white before:transition-all before:duration-400 before:rounded-full
-                                    peer-checked:before:translate-x-6.5">
-                        </span>
-                    </label>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <h3 className="font-semibold mt-1">Show similar products</h3>
-                    <p className="text-sm text-olive-500">People can shop products similar to what's shown in this Pin using visual search
-                    Shopping recommendations aren't available for Idea ads and Pins with tagged products or paid partnership label</p>
-                </div>
-            </div>
-            <div className="relative w-lg">
-                <textarea 
-                    name="can_pin_alt_desc" 
-                    placeholder="Describe your Pin's visual details"
-                    id="can_pin_alt_desc"
-                    className="peer border-gray-300 border rounded-2xl h-18 w-full px-4 pt-7 pb-1 outline-none text-base transition-all resize-none"
-                    />
-                <label
-                    htmlFor="can_pin_alt_desc"
-                    className="absolute left-4 top-2 text-xs font-semibold text-gray-80000 pointer-events-none"
-                    >
-                    Alt Text
-                </label>
-                <p className="text-xs w-full text-center">This helps people using screen readers understand what your Pin is about</p>
-            </div>
         </div>
-    </div>);
+        <div className={`${selectPins ? "w-[20%]" : "w-[5%]"}  border border-olive-300 flex flex-col pt-4`}>
+            {!selectPins  ? (<div className="w-full flex flex-col justify-center">
+                <div className="relative inline-flex items-center justify-center cursor-pointer" onClick={handleSelectPins}>
+                    <Folder className="w-12 h-12 " strokeWidth={1.5}/>
+                    <span className="absolute top-[55%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-xs font-bold">
+                        1
+                    </span>
+                </div>
+                <div className="relative inline-flex items-center justify-center py-4">
+                    <Plus className="w-8 h-8 cursor-pointer"/>
+                </div>
+            </div>) :
+            (<div className="flex flex-col">
+                <div className="flex justify-between items-center px-4 py-2">
+                    <h2 className="font-semibold text-lg">Pin drafts (1)</h2>
+                    <X onClick={handleSelectPins} className="cursor-pointer"/>
+                </div>
+                <div className="flex justify-center">
+                    <button className="font-medium bg-olive-200 w-full rounded-lg mx-4 my-4 py-2">Create a Pin</button>
+                </div>
+                <hr className="mt-2 border-olive-300"/>    
+            </div>)}
+        </div>
+    </div>
+    );
 }
